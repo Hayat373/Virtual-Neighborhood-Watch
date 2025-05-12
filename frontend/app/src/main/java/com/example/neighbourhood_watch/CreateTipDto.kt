be4733f10462961e08debb6ed1237626e937f0c7
Yared Yimenu <yared.yimenu-ug@aau.edu.et>
2025-05-12 16:16:47 +0300

    Add files via upload

---
+++ frontend/app/src/main/java/com/example/neighbourhood_watch/CreateTipDto.kt
@@ -0,0 +1,11 @@
+package com.example.neighbourhood_watch.model
+
+data class CreateTipDto(
+//    val id: Long,
+    val description: String // Change 'tip' to 'description'
+)
+
+data class EditTipDto(
+
+    val description: String // Change 'tip' to 'description'
+)
